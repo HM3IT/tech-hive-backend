24b0701f0fd2a5fdc357d0b52f25a57e6a746b2b
HeinMinMinMaw <heinmin2maw.it@gmail.com>
2024-11-16 11:07:38 +0700

    fix: adjust product permission

--- db/base.py
+++ db/base.py
@@ -19,6 +19,8 @@ AUTH_EXCLUDE_API_ROUTE:list[str] =[
     urls.ACCOUNT_REGISTER,
     product_urls.PRODUCT_LIST,
     product_urls.PRODUCT_DETAIL,
+    product_urls.GET_IMG,
+    product_urls.PRODUCT_ADVANCED_SEARCH,
     "^/schema",
     "^/tests"
 ]
