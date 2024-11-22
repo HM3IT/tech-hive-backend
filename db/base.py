d99f9b4c33eadf787ba65248daeab6329abc435a
HeinMinMinMaw <heinmin2maw.it@gmail.com>
2024-11-22 12:00:37 +0700

    fix: product route API auth fix

--- db/base.py
+++ db/base.py
@@ -20,9 +20,8 @@ AUTH_EXCLUDE_API_ROUTE:list[str] =[
     urls.ACCOUNT_REGISTER,
     urls.ACCOUNT_CREATE,
     category_urls.CATEGORY_LIST, 
-    product_urls.PRODUCT_LIST,
-    product_urls.PRODUCT_DETAIL,
     product_urls.PRODUCT_ADVANCED_SEARCH,
+    r"^/api/products/(list|detail/[^/]+)$",
     r"^/api/files/(?!upload$|update/)[^/]+$",
     "^/schema",
     "^/tests"
